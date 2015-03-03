b843ef4cff1edf90bd42b31cd0c4a29f74dc2b1d
rahul.ranjan <rahul.ranjan@VLD-016.CORP.VALFORMA.COM>
2015-03-03 17:55:29 +0530

    Comitted

---
+++ src/main/java/com/canigenus/common/constants/MailUtil.java
@@ -0,0 +1,128 @@
+package com.canigenus.common.constants;
+
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class MailUtil {
+	
+
+	public static void sendMail(String email, String subject, String message,
+			String from, final String userName, final String password)
+			throws MessagingException {
+
+		// Set the host smtp address
+		Properties props = new Properties();
+		props.put("mail.smtp.host", "smtp.canigenus.com");
+		props.put("mail.smtp.auth", "true");
+
+		Session session = Session.getDefaultInstance(props,
+				new Authenticator() {
+					@Override
+					public PasswordAuthentication getPasswordAuthentication() {
+						return new PasswordAuthentication(userName, password);
+					}
+				});
+
+		session.setDebug(true);
+
+		// create a message
+		Message msg = new MimeMessage(session);
+
+		// set the from and to address
+		InternetAddress addressFrom = new InternetAddress(from);
+		msg.setFrom(addressFrom);
+
+		InternetAddress[] addressTo = new InternetAddress[1];
+		/*
+		 * for (int i = 0; i < recipients.length; i++) {
+		 */
+		addressTo[0] = new InternetAddress(email);
+		/* } */
+		msg.setRecipients(Message.RecipientType.TO, addressTo);
+
+		// Setting the Subject and Content Type
+		msg.setSubject(subject);
+		msg.setContent(message, "text/plain");
+		Transport.send(msg);
+	}
+
+	public static void main(String args[]) throws Exception {
+		
+		String str="<html><body> Thank Your for Signing For Ads Project. Please Click on the Link Below to Activate the Account<br/> <br/> <a href='abc'>Click Here to Activate</a></body></html>";
+		
+		//MailUtil.sendMail("http://localhost:8765/contacts/rest/contact/sendMessageWithBodyHtml/rahulranjanmca@gmail.com","rahulranjanmca@gmail.com", "Subject", str);
+		/*MailUtil.sendMail("donotreply@canigenus.com", "mail@canigenus.com",
+				"password*9", "Subjecrt", "TExt", "smtp", "smtp.canigenus.com",
+				25, null);*/
+		
+		sendMail("admin@javamanor.com", "admin@javamanor.com", "IQBaPSP4", "this is subject", "this is body", "smtp", "smtp.javamanor.com", 465, null);;
+		 
+		System.out.print("I am calling");
+
+	}
+
+	public static void sentMailFromGmailSSL(String to, String from,
+			String password, String subject, String body) {
+		Properties p = new Properties();
+		p.put("mail.smtp.starttls.enable", "true");
+		sendMail(to, from, password, subject, body, "smtps", "smtp.gmail.com",
+				465, p);
+
+	}
+
+	public static void sentMailFromGmailTLS(String to, String from,
+			String password, String subject, String body) {
+		sendMail(to, from, password, subject, body, "smtps", "smtp.gmail.com",
+				465, null);
+	}
+
+	public static void sendMail(String to, String from, String password,
+			String subject, String body, String protocol, String host,
+			int port, Properties properties) {
+		try {
+			Properties propsSSL = new Properties();
+			propsSSL.put("mail.transport.protocol", protocol);
+			propsSSL.put("mail.smtps.host", host);
+			propsSSL.put("mail.smtps.auth", "true");
+			if (properties != null)
+				propsSSL.putAll(properties);
+
+			Session sessionSSL = Session.getInstance(propsSSL);
+
+			sessionSSL.setDebug(true);
+
+			Message messageTLS = new MimeMessage(sessionSSL);
+			messageTLS.setFrom(new InternetAddress(from));
+
+			messageTLS.setRecipients(Message.RecipientType.TO,
+					InternetAddress.parse(to)); // real
+												// recipient
+
+			messageTLS.setSubject(subject);
+			messageTLS.setContent(body, "text/html; charset=utf-8");
+
+			Transport transportTLS = sessionSSL.getTransport();
+
+			transportTLS.connect(host, port, from, password); // account
+																// used
+
+			transportTLS.sendMessage(messageTLS, messageTLS.getAllRecipients());
+
+			transportTLS.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+
+}
---
+++ src/main/java/com/canigenus/common/converter/AbstractObjectConverter.java
@@ -0,0 +1,38 @@
+package com.canigenus.common.converter;
+
+import java.util.List;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+
+import com.canigenus.common.model.Convertible;
+import com.canigenus.common.service.GenericServiceImpl;
+
+public abstract class AbstractObjectConverter implements Converter {
+
+	public abstract GenericServiceImpl<?> getService();
+
+	public String getName() {
+		return "name";
+	}
+
+	public abstract Class<? extends Convertible<?>> getClassType();
+
+	public Object getAsObject(FacesContext context, UIComponent component,
+			String value) {
+		List<? extends Convertible<?>> list = getService().getList(
+				getClassType(), getName(), value);
+		if (list.size() == 0) {
+			return null;
+		} else {
+			return list.get(0);
+		}
+	}
+
+	public String getAsString(FacesContext context, UIComponent component,
+			Object value) {
+		return ((Convertible<?>) value).getName();
+	}
+
+}
