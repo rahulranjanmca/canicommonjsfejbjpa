baa5cf272f55adeaa76a8e0d22ec5979f0696ed1
rahul.ranjan <rahul.ranjan@VLD-016.CORP.VALFORMA.COM>
2015-09-21 12:33:32 +0530

    Committed

--- src/main/java/com/canigenus/common/util/DateUtil.java
+++ src/main/java/com/canigenus/common/util/DateUtil.java
@@ -1,7 +1,11 @@
 package com.canigenus.common.util;
 
 import java.text.DateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
+import java.util.concurrent.TimeUnit;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Named;
@@ -35,6 +39,51 @@ public class DateUtil {
 		str = str + diffMinutes + " minutes, ";
 		str = str + diffSeconds + " seconds.";
 		return str;
+	}
+	
+	
+	public static int same(Date d1, Date d2) {
+		Calendar c1 = Calendar.getInstance();
+		Calendar c2 = Calendar.getInstance();
+		c1.setTime(d1);
+		c2.setTime(d2);
+
+		if (c1.get(Calendar.YEAR) != c2.get(Calendar.YEAR))
+			return c1.get(Calendar.YEAR) - c2.get(Calendar.YEAR);
+		if (c1.get(Calendar.MONTH) != c2.get(Calendar.MONTH))
+			return c1.get(Calendar.MONTH) - c2.get(Calendar.MONTH);
+		return c1.get(Calendar.DAY_OF_MONTH) - c2.get(Calendar.DAY_OF_MONTH);
+
+	}
+	
+	public static Date fromDate(Date date){
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(date);
+		calendar.set(Calendar.HOUR_OF_DAY, 0);
+		calendar.set(Calendar.MINUTE, 0);
+		calendar.set(Calendar.SECOND, 0);
+		return calendar.getTime();
+	}
+	
+	public static Date toDate(Date date){
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(date);
+		calendar.set(Calendar.HOUR_OF_DAY, 23);
+		calendar.set(Calendar.MINUTE, 59);
+		calendar.set(Calendar.SECOND, 59);
+		return calendar.getTime();
+	}
+	
+	public static Date newDate(String timeZone)
+	{
+		Calendar calendar= GregorianCalendar.getInstance();
+		calendar.setTimeZone(TimeZone.getTimeZone(timeZone));
+		return calendar.getTime();
+	}
+	
+	public static long getDateDiff(Date date1, Date date2, TimeUnit timeUnit) {
+	    long diffInMillies = date2.getTime() - date1.getTime();
+	    return timeUnit.convert(diffInMillies,TimeUnit.MILLISECONDS);
 	}
 
 }
